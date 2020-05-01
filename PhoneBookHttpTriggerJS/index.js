9bc288b8c0daf23b846ac8ba1c959ee27f023809
ozawako1 <ozawako1@gmail.com>
2020-05-01 17:34:05 +0900

    rid format_msg

--- PhoneBookHttpTriggerJS/index.js
+++ PhoneBookHttpTriggerJS/index.js
@@ -19,47 +19,6 @@ const CHATPOST_ZOOM_FORMAT = "zoom";
 
 const URL_LUIS_API = "https://eastus.api.cognitive.microsoft.com/luis/v2.0/apps/1c88b3f7-3a27-4769-bd40-7a4c4d1c784e";
 
-function get_garoon_schedules(email){
-
-    return new Promise((resolve, reject) => {
-        var options = {
-            "method": "GET",
-            "uri": process.env.MY_GAROON_GETSCHEDULE_URL,
-            "qs": {
-                "target_user": email
-            },
-            headers: {
-                'User-Agent': 'Request-Promise'
-            },
-            json: true // Automatically parses the JSON string in the response
-        };
-        rp(options)
-            .then((respbody) => resolve(respbody))
-            .catch((err) => reject(err));
-    });
-
-}
-
-function post_chatwork(phonebook, obj, org_msg){
-
-    var total = "";
-
-    for (var i = 0 ; i < phonebook.length ; i++) {
-        var msg = "";
-
-        msg += phonebook[i].name.value + " さんの連絡先は、\n";
-        msg += "内線電話:" + phonebook[i].extensionNumber.value + "\n";
-        msg += "携帯電話:" + phonebook[i].mobilePhone.value + "\n";
-        
-        total += msg;
-        console.log("No." + (i+1) + " " + msg + "\n");
-    }
-
-    total += "\n Prefix: BIZTEL(9) / Osaka(80)";
-
-    obj.Reply(org_msg, total);
-
-}
 
 function get_time(date)
 {
@@ -81,55 +40,62 @@ function isProgress(start, end){
     return ret;
 }
 
-function format_schedule(events){
+function format_phonebook(phonebook){
 
+    var total = "";
+
+    for (var i = 0 ; i < phonebook.length ; i++) {
+        var msg = "";
+
+        msg += phonebook[i].name.value + " さんの連絡先は、\n";
+        msg += "内線電話:" + phonebook[i].extensionNumber.value + "\n";
+        msg += "携帯電話:" + phonebook[i].mobilePhone.value + "\n";
+        
+        total += msg;
+    }
+
+    total += "\n Prefix: BIZTEL(9) / Osaka(80)";
+
+    return total;
+}
+
+function format_schedule(who, schedule){
+
+    var msg1 = who + " さんの予定は、\n";
     var mark = "";
-    var msg = "登録されていません。";
+    var msg2 = "登録されていません。";
 
-    for( var i = 0 ; i < events.length ; i++){
+    for( var i = 0 ; i < schedule.length ; i++){
         
         mark = " ";
         if (i == 0) {
-            msg = "";
+            msg2 = "";
         }
         
-        if (events[i].isAllDay == false) {
-            var st = events[i].start;
-            var et = events[i].end;
+        if (schedule[i].isAllDay == false) {
+            var st = schedule[i].start;
+            var et = schedule[i].end;
             st = !st ? "" : new Date(st.dateTime);
             et = !et ? "" : new Date(et.dateTime);
             if(st != "" && et != "" && isProgress(st, et)) {
                 mark = "*"
             }
-            msg += get_time(st) + "-" + get_time(et) + " " + mark;
+            msg2 += get_time(st) + "-" + get_time(et) + " " + mark;
         }
 
-        msg += events[i].subject + "\n"
+        msg2 += schedule[i].subject + "\n"
     }
 
-    return msg;
-
+    return msg1 + msg2;
 }
 
-function post_chatwork_(who, schedules, obj, org_msg){
-    
-    var msg1 = who + " さんの予定は、\n";
-    var msg2 = format_schedule(schedules);
-    obj.Post(org_msg, msg1 + msg2);
-    
-    return new Promise((resolve, reject) => {
-        resolve();
-    });
-}
-
-
-function get_zoommeetings(zoom_user_id){
+function getLiveZoomMeeting(zoom_user_email){
 
     return new Promise((resolve, reject) => {
         var op = {
             uri: process.env.MY_ZOOM_GETMEETINGS_URL,
             qs: {
-                zuid: zoom_user_id
+                zmail: zoom_user_email
             },
             json: true
         };
@@ -140,53 +106,67 @@ function get_zoommeetings(zoom_user_id){
 
 }
 
-function format_mtg_z(mtg)
-{
-    var fmt = "" + mtg.topic + "\n";
-    if (mtg.start_time != undefined) {
-        var dtime = moment(mtg.start_time).tz(mtg.timezone);
-        fmt += "時間: " + dtime.format("YYYY/MM/DD HH:mm");
-        if (mtg.duration != undefined && mtg.duration > 0) {
+function getGaroonSchedule(email){
 
-            fmt += " - " + dtime.add(mtg.duration, 'm').format("HH:mm");
-        }
-        fmt += "\n";
-    }
-    if (mtg.join_url != undefined) {
-        fmt += "参加URL: " + mtg.join_url + " \n";
-    }
-    return fmt;
+    return new Promise((resolve, reject) => {
+        var options = {
+            "method": "GET",
+            "uri": process.env.MY_GAROON_GETSCHEDULE_URL,
+            "qs": {
+                "target_user": email
+            },
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+        rp(options)
+            .then((respbody) => resolve(respbody))
+            .catch((err) => reject(err));
+    });
+
 }
 
-function post_chatwork3(results, who, obj, org_msg){
+function format_meetings(Who, Meetings){
 
     var now = "";
-    var next = "";
 
-    for (var i = 0 ; i < results.length ; i++){
+    for (var i = 0 ; i < Meetings.length ; i++){
         var mtg = results[i];
-        if (mtg.live == "true"){
-            now += "現在、"+ who +" で以下のMTGが開催されています。\n";
-            now += format_mtg_z(mtg);
-            now += "\n";
-        } else {
-            if(next == ""){
-                next = "今後、以下のMTGが予定されています。\n";
+
+        now += "現在、"+ Who +" で以下のMTGが開催されています。\n";
+        
+        //タイトル
+        now += mtg.topic + "\n";
+        //時刻設定
+        if (mtg.start_time != undefined) {
+            var dtime = moment(mtg.start_time).tz(mtg.timezone);
+            now += "時間: " + dtime.format("YYYY/MM/DD HH:mm");
+            //所要時間指定あり
+            if (mtg.duration != undefined && mtg.duration > 0) {
+                now += " - " + dtime.add(mtg.duration, 'm').format("HH:mm");
             }
-            next += format_mtg_z(mtg);
+            now += "\n";
         }
+        // 参加URL
+        if (mtg.join_url != undefined) {
+            now += "参加URL: " + mtg.join_url + " \n";
+        }
+
     }
+
     if (now == ""){
-        now = "現在、"+ who +" で、開催中のMTGはありません。\n\n";
+        now = "現在、"+ Who +" で、開催中のMTGはありません。\n\n";
     }
 
-    if (now != "" || next != "") {
-        obj.Reply(org_msg, now + next);
-    }
-
-    return results;
+    return now;
 }
 
+function reply_chatwork(obj, org_msg, reply_msg){
+    
+    return obj.Post(org_msg, reply_msg);
+
+}
 
 function send_sorry(err, obj, org_msg) {
     
@@ -314,15 +294,19 @@ module.exports = function (context, req) {
                     }
                     phonebook = results;
                 })
-                .then(() => post_chatwork(phonebook, obj, msg))
+                .then(() => format_phonebook(phonebook))
+                .then((formatted) => reply_chatwork(obj, msg, formatted))
                 .then(() => {
                     if (singleuser) {
                         obj.is_internal_user(cdb, msg.from_id)
                         .then((internal) => {
                             if (internal) {
-                                get_garoon_schedules(target)
-                                .then((schedule) => post_chatwork_(fullname, schedule, obj, msg))
-                                .catch((err) => post_chatwork_(fullname, [], obj, msg));    
+                                getGaroonSchedule(target)
+                                .then((schedule) => format_schedule(fullname, schedule))
+                                .then((formatted) => reply_chatwork(obj, msg, formatted))
+                                .catch(function(err) {
+                                    send_sorry(err, obj, msg);
+                                });        
                             }
                         })
                         .catch(function(err){
@@ -344,11 +328,12 @@ module.exports = function (context, req) {
                 obj.is_internal_user(cdb, msg.from_id)
                 .then((internal) => {
                     if (internal) {
-                        sql.query_zoomusers(email)
-                        .then((results) => get_zoommeetings(results[0].id.value.trim()))
-                        .then((results) => post_chatwork3(results, who, obj, msg))
-                        .then(() => get_garoon_schedules(email))
-                        .then((results) => post_chatwork_(who, results, obj, msg))
+                        getLiveZoomMeeting(email)
+                        .then((meetings) => format_meetings(who, meetings))
+                        .then((formatted) => reply_chatwork(obj, msg, formatted))
+                        .then(() => getGaroonSchedule(email))
+                        .then((schedule) => format_schedule(who, schedule))
+                        .then((formatted) => reply_chatwork(obj, msg, formatted))
                         .catch(function(err) {
                             send_sorry(err, obj, msg);
                         });
